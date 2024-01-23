838f7e8df8d6f4937a3f076167d646863b81aa64
Daniela <danielamr@192.168.1.9>
2024-01-23 08:57:01 -0500

    implements openai request and save that data response

---
+++ src/components/PromptToLocation.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+
+const PromptToLocation = (prompt) => {
+  const url = "https://aims.hubwtqa.com/api/v1/chat";
+
+  const data = {
+    model: "gpt-3.5-turbo-0613",
+    messages: [{ role: "user", content: prompt }],
+  };
+  const params = {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    method: "POST",
+  };
+  
+  return fetch(url, params)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+      return Promise.reject(
+        "Unable to identify a location from your question. Please try again."
+      );
+    });
+};
+
+PromptToLocation.propTypes = {
+  prompt: PropTypes.string.isRequired,
+};
+
+export default PromptToLocation;
